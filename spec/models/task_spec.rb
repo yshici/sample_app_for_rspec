c1fc3e53396f66533ce2f1a5cdeebaefd2c8a311
yshici <lo.ol.000.yt@gmail.com>
2020-08-28 13:38:02 +0900

    [Fix] 変数名を内容を反映したものに変更

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,31 +3,33 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'validation' do
     it 'タイトル、ステータスがあれば有効な状態であること' do
-      expect(build(:task)).to be_valid
+      task = build(:task)
+      expect(task).to be_valid
     end
 
     it 'タイトルが無ければ無効な状態であること' do
-      task = build(:task, title: nil)
-      task.valid?
-      expect(task.errors[:title]).to include("can't be blank")
+      task_without_title = build(:task, title: nil)
+      task_without_title.valid?
+      expect(task_without_title.errors[:title]).to include("can't be blank")
     end
 
     it 'ステータスが無ければ無効な状態であること' do
-      task = build(:task, status: nil)
-      task.valid?
-      expect(task.errors[:status]).to include("can't be blank")
+      task_without_status = build(:task, status: nil)
+      task_without_status.valid?
+      expect(task_without_status.errors[:status]).to include("can't be blank")
     end
 
     it '重複したタイトルなら無効な状態であること' do
-      create(:task, title: 'test')
-      task = build(:task, title: 'test')
-      task.valid?
-      expect(task.errors[:title]).to include("has already been taken")
+      task = create(:task)
+      task_with_duplicated_title = build(:task, title: task.title)
+      task_with_duplicated_title.valid?
+      expect(task_with_duplicated_title.errors[:title]).to include("has already been taken")
     end
 
     it '重複しないタイトルなら有効な状態であること' do
-      create(:task)
-      expect(build(:task)).to be_valid
+      task = create(:task)
+      task_with_another_title = build(:task, title: 'another_title')
+      expect(task_with_another_title).to be_valid
     end
   end
 end
