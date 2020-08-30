abcef235c7fef4b38221d53174682e44fde579c3
yshici <lo.ol.000.yt@gmail.com>
2020-08-30 20:26:15 +0900

    [Fix] タスクに関するSydtemSpecテスト項目追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -20,6 +20,23 @@ RSpec.describe "Tasks", type: :system do
         expect(current_path).to eq login_path
       end
     end
+    context 'タスクの詳細ページにアクセス' do
+      it 'タスクの詳細情報が表示される' do
+        visit task_path(task)
+        expect(page).to have_content task.title
+        expect(current_path).to eq task_path(task)
+      end
+    end
+    context 'タスクの一覧ページにアクセス' do
+      it 'すべてのユーザーのタスク情報が表示される' do
+        task_list = create_list(:task, 3)
+        visit tasks_path
+        expect(page).to have_content task_list[0].title
+        expect(page).to have_content task_list[1].title
+        expect(page).to have_content task_list[2].title
+        expect(current_path).to eq tasks_path
+      end
+    end
   end
 
 
